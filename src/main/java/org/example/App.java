ee1357dc7b14262204c8eae127cbf18639249b22
Quentin QM. MARTEL <martel@NORDON.LOCAL>
2024-06-18 20:05:26 +0200

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,13 @@
+package org.example;
+
+/**
+ * Hello world!
+ *TODO faire sont code
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
